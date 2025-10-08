b40e01a49f9d0da97e4bb8e06ef1425d2aea4f54
Akhil Padackal Vijayan <akhil.p@omaemirates.com>
2025-10-08 11:18:16 +0400

    removed unwanted changes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-console.log('API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
